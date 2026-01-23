2651d5abe470a46624ae7775fa45e3485b8f7f4c
Ramalh0VT <gustavoramalhodasilva777@gmail.com>
2026-01-23 17:08:59 -0300

    Learned alot about object oriented programming

---
+++ Python/object_oriented_programming/Projects/virtual_pet_simulator.py
@@ -0,0 +1,27 @@
+class Pet:
+    def __init(self, name, type, hunger, hygiene, happiness, health):
+
+        self.name = name
+        elf.type = type
+        self.hunger = hunger
+        self.hygiene = hygiene
+        self.happiness = happiness
+        self.health = health
+
+        hunger = 50
+        hygiene = 50
+        happiness = 50
+        health = 50
+
+    def feed(self):
+        self.hunger = hunger + 20
+        self.health = health + 5
+
+
+
+
+
+    
+
+
+    
