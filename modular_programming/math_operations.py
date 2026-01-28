ed12bcf775a1bf6f560df312ca0ee661b84aec42
Ramalh0VT <gustavoramalhodasilva777@gmail.com>
2026-01-28 12:35:59 -0300

    Started a calculator project to learn modules

---
+++ modular_programming/math_operations.py
@@ -0,0 +1,16 @@
+def addition(num1, num2):
+    final_result = num1 + num2
+    return final_result
+
+def subtraction(num1, num2): 
+    final_result = num1 - num2
+    return final_result
+
+def multiplication(num1, num2):
+    final_result = num1 * num2
+    return final_result
+
+def division (num1, num2):
+    final_result = num1 / num2
+    return final_result
+
